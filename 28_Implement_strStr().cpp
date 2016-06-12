9190d903005b76389157e87d39d73a87ef086f05
ccyxf <865066065@qq.com>
2016-06-12 15:15:23 +0800

    Create 28_Implement_strStr().cpp

---
+++ 28_Implement_strStr().cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()==0) return 0;
+        else if(haystack.size()==0) return -1;
+        
+        for(int i=0;;i++){
+            for(int j=0;;j++){
+                if(j==needle.size()) return i;
+                if(i+j==haystack.size()) return -1;
+                if(haystack[i+j]!=needle[j]) break;
+            }
+        }
+    }
+};
